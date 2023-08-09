b0dece145175d4ec96dee84f39267989138db245
Peter James Winn <41901620+PeterJamesWinn@users.noreply.github.com>
2023-08-09 19:59:13 +0000

    Copilot third commit - I don't programme javascript so this is probably rubbish!

---
+++ comments.js
@@ -0,0 +1,7 @@
+//Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var ROOT_DIR = "html/";
+var port = 8080;
+var comments = [{author: "Dale", text: "Hello
